20a36f7321bea80a308d660e7a4f8dba750f0240
Jérôme Coupé <jercoupe@gmail.com>
2026-07-16 09:59:02 +0200

    Homepage WIP

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,4 @@
 import { init as nav } from "./modules/navigation.js";
 nav();
 
-import { init as modeswitcher } from "./modules/modeswitcher.js";
-modeswitcher();
+import "./components/modeswitcher.js";
